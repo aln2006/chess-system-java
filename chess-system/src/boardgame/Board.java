357534fcc1a63eb1737a92f5e8098c73bc026555
Alan Leão <alanparnaiba2006@gmail.com>
2024-08-15 15:11:43 -0300

    Chess layer and print the board

--- chess-system/src/boardgame/Board.java
+++ chess-system/src/boardgame/Board.java
@@ -5,12 +5,11 @@ public class Board {
     private int columns;
     private Piece[][] pieces;
 
-    public Board(int rows, int columns){
-        this.columns = columns;
+    // Construtor para inicializar o tabuleiro
+    public Board(int rows, int columns) {
         this.rows = rows;
-
-        pieces = new Piece[rows][columns];
-
+        this.columns = columns;
+        pieces = new Piece[rows][columns]; // Inicializa a matriz de peças
     }
 
     public int getColumns() {
@@ -21,11 +20,11 @@ public class Board {
         return rows;
     }
 
-    public void setColumns(int columns) {
-        this.columns = columns;
+    public Piece piece(int row, int column) {
+        return pieces[row][column];
     }
 
-    public void setRows(int rows) {
-        this.rows = rows;
+    public Piece piece(Position position) {
+        return pieces[position.getRow()][position.getColumn()];
     }
 }
