1ce882ae02d19bb38c90e186ab62f68fb511be2e
Alan Leão <alanparnaiba2006@gmail.com>
2024-08-15 15:49:11 -0300

    Add the exceptions of the Board, and defensive programing

--- chess-system/src/boardgame/Board.java
+++ chess-system/src/boardgame/Board.java
@@ -7,6 +7,9 @@ public class Board {
 
     // Construtor para inicializar o tabuleiro
     public Board(int rows, int columns) {
+        if (rows <1 || columns < 1){
+            throw new BoardException("Error creating board: there must to be at least 1 row and 1 column");
+        }
         this.rows = rows;
         this.columns = columns;
         pieces = new Piece[rows][columns]; // Inicializa a matriz de peças
@@ -21,16 +24,42 @@ public class Board {
     }
 
     public Piece piece(int row, int column) {
+        if (!positionExistis(row, column)){
+            throw new BoardException("Position not on the board");
+        }
         return pieces[row][column];
     }
 
     public Piece piece(Position position) {
+        if (!positionExistis(position)){
+            throw new BoardException("Position not on the board");
+        }
         return pieces[position.getRow()][position.getColumn()];
     }
 
     public void PlacePiece(Piece piece, Position position){
+        if (thereIsAPiece(position)){
+            throw new BoardException("There is already a piece in this position");
+        }
         pieces[position.getRow()][position.getColumn()] = piece;
         piece.position = position;
 
+    }
+
+    private boolean positionExistis(int row, int column){
+       return row>=0 && row < rows && column >= 0 && column <= columns;
+
+    }
+
+    public boolean positionExistis(Position position){
+        return positionExistis(position.getRow(), position.getColumn());
+
+    }
+
+    public boolean thereIsAPiece(Position position){
+        if (!positionExistis(position)){
+            throw new BoardException("Position not on the board");
+        }
+        return piece(position) != null;
     }
 }
